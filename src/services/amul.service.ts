0fcd8d304f051819ee56f6af7bcd6cd78e051528
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2025-06-22 16:12:34 +0530

    initialize using distinct pincodes

--- src/services/amul.service.ts
+++ src/services/amul.service.ts
@@ -2,37 +2,18 @@ import { getOrCreateAmulApi } from '@/libs/amulApi.lib'
 import UserModel, { IUser } from '@/models/user.model'
 import { sleep } from '@/utils'
 import { FilterQuery } from 'mongoose'
+import { getDistinctPincodes } from './user.service'
 
 export const initiateAmulSessions = async () => {
-  const PAGE_SIZE = 1000
+  const distinctPincodes = await getDistinctPincodes()
 
-  const query: FilterQuery<IUser> = {
-    pincode: { $exists: true, $ne: null },
-    substore: { $exists: true, $ne: null }
-  }
-  const totalUsers = await UserModel.countDocuments(query)
-  const totalPages = Math.ceil(totalUsers / PAGE_SIZE)
-
-  for (let page = 0; page < totalPages; page++) {
-    const users = await UserModel.find(query)
-      .skip(page * PAGE_SIZE)
-      .limit(PAGE_SIZE)
-      .select('pincode substore')
-      .lean()
-
-    for (const user of users) {
-      try {
-        await getOrCreateAmulApi(user.pincode)
-        console.log(
-          `Initiated session for user with pincode: ${user.pincode}, substore: ${user.substore}`
-        )
-        await sleep(10 * 1000) // Sleep for 30 seconds between each session initiation
-      } catch (err) {
-        console.error(
-          `Failed to initiate session for user with pincode: ${user.pincode}, substore: ${user.substore}`,
-          err
-        )
-      }
+  for (const pincode of distinctPincodes) {
+    try {
+      await getOrCreateAmulApi(pincode)
+      console.log(`Initiated session for pincode: ${pincode}`)
+      await sleep(10 * 1000) // Sleep for 30 seconds between each session initiation
+    } catch (err) {
+      console.error(`Failed to initiate session for pincode: ${pincode}`, err)
     }
   }
 }
