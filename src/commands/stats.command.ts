a9bdd666270867600bf27736dfa8abfadfe22548
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2025-06-22 16:08:19 +0530

    distinct data stats

--- src/commands/stats.command.ts
+++ src/commands/stats.command.ts
@@ -1,6 +1,10 @@
 import { substoreSessions } from '@/libs/amulApi.lib'
 import ProductModel from '@/models/product.model'
 import UserModel from '@/models/user.model'
+import {
+  getDistinctPincodes,
+  getDistinctSubstores
+} from '@/services/user.service'
 import { CommandContext } from '@/types/context.types'
 import { MiddlewareFn } from 'telegraf'
 
@@ -9,9 +13,8 @@ export const statsCommand: MiddlewareFn<CommandContext> = async (ctx, next) => {
   const totalTracked = await ProductModel.countDocuments()
 
   const totalAmulSessions = substoreSessions.size
-  const totalUniqueSubstores = new Set(
-    Array.from(substoreSessions.keys()).map((session) => session.substore)
-  ).size
+  const distinctSubstores = await getDistinctSubstores()
+  const distinctPincodes = await getDistinctPincodes()
 
   await ctx.reply(
     [
@@ -19,7 +22,8 @@ export const statsCommand: MiddlewareFn<CommandContext> = async (ctx, next) => {
       `Total Users: <b>${totalUsers}</b>`,
       `Total Tracked Products: <b>${totalTracked}</b>`,
       `Total Amul Sessions: <b>${totalAmulSessions}</b>`,
-      `Total Unique Substores: <b>${totalUniqueSubstores}</b>`
+      `Total Unique Substores: <b>${distinctSubstores.length}</b>`,
+      `Total Unique Pincodes: <b>${distinctPincodes.length}</b>`
     ].join('\n'),
     {
       parse_mode: 'HTML'
