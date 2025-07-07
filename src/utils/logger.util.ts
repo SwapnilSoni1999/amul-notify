2c129d5defa467dc8007d2f0adbd08e29e3b1542
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2025-07-07 11:22:16 +0530

    chore: added search query docs for /products command

--- src/utils/logger.util.ts
+++ src/utils/logger.util.ts
@@ -14,5 +14,11 @@ export const logToChannel = async (text: string, next?: () => void) => {
     next?.()
   } catch (err) {
     console.error('Error in logToChannel:', err)
+    logToChannel(
+      `Error in logToChannel: ${
+        err instanceof Error ? err.message : String(err)
+      }`,
+      next
+    )
   }
 }
