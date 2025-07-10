0b397b3f22492b521b9498806e5c5f570bd9e8bd
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2025-07-10 11:25:04 +0530

    express: start server

--- src/server.ts
+++ src/server.ts
@@ -15,58 +15,61 @@ redis.on('error', (err) => {
 
 mongoose
   .connect(env.MONGO_URI)
-  .then(() => {
+  .then(async () => {
     console.log('Connected to MongoDB successfully')
-    app.listen(env.PORT, async () => {
-      console.log(`Server is running on port ${env.PORT}`)
 
-      initiateAmulSessions()
+    initiateAmulSessions()
 
-      if (env.BOT_WEBHOOK_URL) {
-        const botSecret = `amul_${bot.secretPathComponent()}`
-        console.log(`Setting webhook to: ${env.BOT_WEBHOOK_URL}`)
-        await bot.telegram.setWebhook(env.BOT_WEBHOOK_URL, {
-          secret_token: botSecret
+    if (env.BOT_WEBHOOK_URL) {
+      const botSecret = `amul_${bot.secretPathComponent()}`
+      console.log(`Setting webhook to: ${env.BOT_WEBHOOK_URL}`)
+      await bot.telegram.setWebhook(env.BOT_WEBHOOK_URL, {
+        secret_token: botSecret
+      })
+
+      const url = new URL(env.BOT_WEBHOOK_URL)
+      const hookPath = url.pathname.endsWith('/')
+        ? url.pathname.slice(0, -1)
+        : url.pathname
+
+      app.use(hookPath, (req, res) => {
+        // console.log('Bot Secret:', req.get('X-Telegram-Bot-Api-Secret-Token'))
+        if (req.get('X-Telegram-Bot-Api-Secret-Token') !== botSecret) {
+          console.error('Invalid secret token')
+          return res.status(403).end()
+        }
+
+        bot.handleUpdate(req.body, res).catch((err) => {
+          console.error('Error in bot.handleUpdate:', err)
+          // Still respond 200 so Telegram won’t retry endlessly
+          res.status(200).end()
         })
+      })
 
-        const url = new URL(env.BOT_WEBHOOK_URL)
-        const hookPath = url.pathname.endsWith('/')
-          ? url.pathname.slice(0, -1)
-          : url.pathname
-
-        app.use(hookPath, (req, res) => {
-          // console.log('Bot Secret:', req.get('X-Telegram-Bot-Api-Secret-Token'))
-          if (req.get('X-Telegram-Bot-Api-Secret-Token') !== botSecret) {
-            console.error('Invalid secret token')
-            return res.status(403).end()
-          }
-
-          bot.handleUpdate(req.body, res).catch((err) => {
-            console.error('Error in bot.handleUpdate:', err)
-            // Still respond 200 so Telegram won’t retry endlessly
-            res.status(200).end()
-          })
+      app.listen(env.PORT, () => {
+        console.log(`Server is running on port ${env.PORT}`)
+      })
+      console.log('Bot is running with webhook mode...')
+      console.log(await bot.telegram.getWebhookInfo())
+    } else {
+      bot
+        .launch(() => {
+          console.log('Bot is running...')
         })
-      } else {
-        bot
-          .launch(() => {
-            console.log('Bot is running...')
-          })
-          .catch((err) => {
-            console.error('Failed to launch bot:', err)
-          })
-      }
+        .catch((err) => {
+          console.error('Failed to launch bot:', err)
+        })
+    }
 
-      // Start job
-      if (env.TRACKER_ENABLED) {
-        console.log('Starting stock checker job...')
-        stockCheckerJob.start()
-        stockCheckerJob.execute()
-        console.log('Stock checker job started')
-      } else {
-        console.log('Stock tracker is disabled. Skipping job execution.')
-      }
-    })
+    // Start job
+    if (env.TRACKER_ENABLED) {
+      console.log('Starting stock checker job...')
+      stockCheckerJob.start()
+      stockCheckerJob.execute()
+      console.log('Stock checker job started')
+    } else {
+      console.log('Stock tracker is disabled. Skipping job execution.')
+    }
   })
   .catch((err) => {
     console.error('Failed to connect to MongoDB:', err)
