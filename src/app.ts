292ea9b443f791e0c289d770afe789b89033c4bd
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2025-07-11 16:41:56 +0530

    feat: added badge endpoint

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
 import env from './env'
+import UserModel from './models/user.model'
 
 const app = express()
 
@@ -13,5 +14,15 @@ app.use(morgan(env.NODE_ENV !== 'production' ? 'dev' : 'combined'))
 if (env.NODE_ENV === 'production') {
   app.enable('trust proxy')
 }
+
+app.use('/amul-bot/badge', async (req, res) => {
+  const count = await UserModel.countDocuments({})
+  res.json({
+    schemaVersion: 1,
+    label: 'users',
+    message: String(count),
+    color: 'blue'
+  })
+})
 
 export default app
