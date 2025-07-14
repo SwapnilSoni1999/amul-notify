fd5ba491a5529b5ea13270cdff87991810ac28d9
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2025-07-14 13:32:15 +0530

    Revert "temp: do not show settings btn in menu as of now"
    This reverts commit 49228c631fef2aa6aef9552985fb8a77d3df1b47.

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ export const userCommands: readonly BotCommand[] = [
   { command: 'settings', description: 'View or change your settings' },
   { command: 'tracked', description: 'List all tracked products' },
   { command: 'favourites', description: 'List your favourite products' },
-  // { command: 'settings', description: 'View or change your settings' },
+  { command: 'settings', description: 'View or change your settings' },
   { command: 'support', description: 'Get support' }
 ]
 
