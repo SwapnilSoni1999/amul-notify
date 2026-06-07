d07f12889a8eaaa409a26029dab5ce3385ded76c
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2026-06-07 12:40:54 -0400

    fix: formatting

--- src/jobs/checker.job.ts
+++ src/jobs/checker.job.ts
@@ -285,7 +285,15 @@ const stockCheckerJob = schedule(
 
             const message = [
               `${emojis.fire} <b>Product Update: ${product.name}</b>`,
-              formatProductDetails(product, isAvailablForPurchase, 0, undefined, undefined, user.pincode, user.substore),
+              formatProductDetails(
+                product,
+                isAvailablForPurchase,
+                0,
+                undefined,
+                undefined,
+                user.pincode,
+                user.substore
+              ),
               '',
               await getInfoMessage()
               // Show untracked info
