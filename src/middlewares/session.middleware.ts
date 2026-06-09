f899f2bb2df10dca2ba70421c7f3a3dc502bb0b8
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2026-06-08 23:18:11 -0400

    fix: eslint

--- src/middlewares/session.middleware.ts
+++ src/middlewares/session.middleware.ts
@@ -4,7 +4,7 @@ import { MyContext } from '@/types/context.types'
 import { emojis } from '@/utils/emoji.util'
 import ProductModel, { HydratedProduct } from '@/models/product.model'
 import { AmulApi, getOrCreateAmulApi } from '@/libs/amulApi.lib'
-import { isLoggedIn } from '@/utils/autoOrder.util'
+// import { isLoggedIn } from '@/utils/autoOrder.util'
 
 export const sessionMiddleware: MiddlewareFn<MyContext> = async (ctx, next) => {
   if (!ctx.from) {
