7a93bdf1fb4867466afda6cf3286f5dbc201306c
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2026-06-23 21:53:39 -0400

    Revert "temp: drop pending updates"
    This reverts commit f29d89a302c8e3da76123392849904672f36b237.

--- src/server.ts
+++ src/server.ts
@@ -30,8 +30,7 @@ mongoose
       const botSecret = `amul_${bot.secretPathComponent()}`
       console.log(`Setting webhook to: ${env.BOT_WEBHOOK_URL}`)
       await bot.telegram.setWebhook(env.BOT_WEBHOOK_URL, {
-        secret_token: botSecret,
-        drop_pending_updates: true
+        secret_token: botSecret
       })
 
       const url = new URL(env.BOT_WEBHOOK_URL)
@@ -59,14 +58,9 @@ mongoose
       console.log(await bot.telegram.getWebhookInfo())
     } else {
       bot
-        .launch(
-          {
-            dropPendingUpdates: true
-          },
-          () => {
-            console.log('Bot is running...')
-          }
-        )
+        .launch(() => {
+          console.log('Bot is running...')
+        })
         .catch((err) => {
           console.error('Failed to launch bot:', err)
         })
